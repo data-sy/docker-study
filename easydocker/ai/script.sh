db2ac1b3d843b5097f8f346054daa3f207cc5fca
Lee Soyeon <data.sy.2@gmail.com>
2024-02-02 19:23:59 +0900

    플라스크 서버 성공!!
    - 맥에서는 5000을 사용하고 있기 때문에 호스트포트번호는 5000 말고 8000
    - 도커에서 플라스크에 할당해둔 포트번호가 5000 이기 때문에 다른 번호 사용하면 curl: (56) Recv failure: Connection reset by peer 에러
    - 결론 : 8000:5000

---
+++ easydocker/ai/script.sh
@@ -0,0 +1,9 @@
+# Docker 이미지 빌드 및 푸시
+docker build -t mmt2024/mmt-ai:1.0.0 . --no-cache
+docker push mmt2024/mmt-ai:1.0.0
+
+# 컨테이너 생성 (윈도우 cmd or Powershell 사용)
+docker run -d -p 8000:5000 --network mmt-network --name mmt-ai mmt2024/mmt-ai:1.0.0
+
+# 로그 확인 및 http://localhost 접속
+docker logs -f mmt-ai
