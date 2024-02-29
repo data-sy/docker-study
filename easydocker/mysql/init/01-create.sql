fb1feb07fd92de10f9ab4bc49c07de7618148f1c
Lee Soyeon <data.sy.2@gmail.com>
2024-02-29 20:49:28 +0900

    docker pull 없애도 됨 (어차피 컴포즈에서 풀 받으니까)

--- easydocker/mysql/init/01-create.sql
+++ easydocker/mysql/init/01-create.sql
@@ -162,3 +162,4 @@ INSERT INTO user_authority (user_id, authority_name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO user_authority (user_id, authority_name) VALUES (2, 'ROLE_USER');
 
 -- CD 테스트
+-- CD 테스트
