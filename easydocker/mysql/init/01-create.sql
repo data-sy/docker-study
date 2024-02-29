672ee0e052b2d95bd592340641f24f0fd64964b0
Lee Soyeon <data.sy.2@gmail.com>
2024-02-29 20:23:57 +0900

    왜 수정됐는데 액션 실행이 안 되지?

--- easydocker/mysql/init/01-create.sql
+++ easydocker/mysql/init/01-create.sql
@@ -160,6 +160,3 @@ INSERT INTO authority (authority_name) VALUES ('ROLE_ADMIN');
 INSERT INTO user_authority (user_id, authority_name) VALUES (1, 'ROLE_USER');
 INSERT INTO user_authority (user_id, authority_name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO user_authority (user_id, authority_name) VALUES (2, 'ROLE_USER');
-
--- cd 테스트!!!!
--- cd 테스트!!!!
