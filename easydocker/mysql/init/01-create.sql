6a759f322f7b8f024ee8c6676df779a836bf12ae
Lee Soyeon <data.sy.2@gmail.com>
2024-02-29 19:22:35 +0900

    ec2에 도커 컴포즈 설치 후 재시도

--- easydocker/mysql/init/01-create.sql
+++ easydocker/mysql/init/01-create.sql
@@ -161,4 +161,4 @@ INSERT INTO user_authority (user_id, authority_name) VALUES (1, 'ROLE_USER');
 INSERT INTO user_authority (user_id, authority_name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO user_authority (user_id, authority_name) VALUES (2, 'ROLE_USER');
 
--- cd 테스트!
+-- cd 테스트!!
