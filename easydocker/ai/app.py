4cbbbb17c22af415fa3568c81adb6296f482daf8
Lee Soyeon <data.sy.2@gmail.com>
2024-03-02 12:53:16 +0900

    ai, api 깃허브액션테스트

--- easydocker/ai/app.py
+++ easydocker/ai/app.py
@@ -9,7 +9,7 @@ from predict import predict
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 # CORS(app, resources={r"/*": {"origins": ["http://localhost", "http://localhost:8080"]}})
-
+# CD 테스트
 @app.route('/')
 def hello_world():
     return 'Hello world!'
