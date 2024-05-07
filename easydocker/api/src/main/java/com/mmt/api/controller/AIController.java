f61596956fcff3a216c096a823fd1a9eaec0090a
Lee Soyeon <data.sy.2@gmail.com>
2024-05-07 20:51:43 +0900

    깃허브액션 ECR 테스트

--- easydocker/api/src/main/java/com/mmt/api/controller/AIController.java
+++ easydocker/api/src/main/java/com/mmt/api/controller/AIController.java
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/ai")
 public class AIController {
 
-    // 깃허브 액션 테스트
     // 깃허브 액션 ECR 테스트 
-
+    
     private final AnswerService answerService;
     private final ProbabilityService probabilityService;
 
