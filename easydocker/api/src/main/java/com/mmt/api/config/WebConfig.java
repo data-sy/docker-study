ba2e6df2f92aec2c2548aead02a9a98c6bde9da0
Lee Soyeon <data.sy.2@gmail.com>
2024-05-14 18:29:37 +0900

    스프링에서도 컨테이너 이름 바꾸기

--- easydocker/api/src/main/java/com/mmt/api/config/WebConfig.java
+++ easydocker/api/src/main/java/com/mmt/api/config/WebConfig.java
@@ -11,9 +11,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             //    .allowedOrigins("/*") // 외부에서 들어오는 모든 url 허용
+            // 로컬
+                // .allowedOrigins(
+                //     "http://localhost:8080", "http://localhost", "http://localhost:80", "http://localhost:8000", 
+                //     "http://mmt-backend:8080", "http://mmt-front:80", "http://mmt-ai:5000")
+            // EC2
                 .allowedOrigins(
                     "http://localhost:8080", "http://localhost", "http://localhost:80", "http://localhost:8000", 
-                    "http://mmt-backend:8080", "http://mmt-front", "http://mmt-front:80", "http://mmt-ai:5000")
+                    "http://ec2-user-mmt-backend-1:8080", "http://ec2-user-mmt-front-1:80", "http://ec2-user-mmt-ai-1:5000")                
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true) // 클라이언트에서 쿠키를 받기 위해
